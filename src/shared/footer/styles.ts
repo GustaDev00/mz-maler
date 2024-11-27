f2a05db5932d65de4fef1e4cf59f2e05be237a58
Gustavo pontes <gustavo.4dev@gmail.com>
2024-11-27 01:10:28 -0300

    feat: apagando a imagem

--- src/shared/footer/styles.ts
+++ src/shared/footer/styles.ts
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { Logo as _Logo } from "@/components/atoms/logo";
 import { Wrapper as _Wrapper } from "@/styles/components/wrapper";
 import { mediaMaxWidth } from "@/utils/media-query";
